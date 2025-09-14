48b5a3f6156a74842da3a6b434528be87bf312a4
Pedro Correia Cravo Pimenta <pedroccpimenta@gmail.com>
2025-09-14 21:18:01 +0100

    :gear: update

---
+++ README.md
@@ -0,0 +1,30 @@
+# AIM4M Tools and Techniques for Geospatial visualization
+
+## Summary
+This capstone project demonstrates the practical application of geospatial visualization techniques learned through the "Tools and Techniques for Geospatial ML - AIM4Mobility" program at Factual Academy.
+
+## Foreword
+This analysis investigates several key questions related to the distribution and characteristics of reported accidents throughout the year:
+
+1. Seasonal Trends: Is there a noticeable variation in the number of accidents across different months? Specifically, do accident rates increase during the winter months or decrease in the summer, or vice versa?
+
+2. Temporal Patterns: How does the frequency of accidents vary by both the day of the week and the hour of the day? What times of day exhibit the highest incidence of accidents? Are there distinct patterns between weekdays and weekends?
+
+3. Spatial Dynamics: Does the geographic distribution of accidents change with the seasons? In particular, do accident 'hotspots' shift locations within the city depending on whether it's winter or summer?
+
+## Answers
+1. Seasonal Dependency:
+There is no meaningful dependency between the number of accidents and the month of the year. The lowest accident counts occur in February, August, and December. Notably, if February’s count is adjusted to account for its shorter duration (estimating the total if the month had 31 days), its accident count (157) becomes similar to those of August (158) and December (155), suggesting that monthly differences are minimal and largely explained by the differing number of days rather than seasonality.
+
+2. Temporal Patterns (Weekday & Time):
+The heatmap showing accident counts as a function of weekday and hour reveals a concentration of accidents towards the end of the day, especially on Thursdays and Fridays. This suggests higher risk during late weekday hours, possibly due to increased activity and travel at those times.
+
+3. Spatial Dynamics:
+This Streamlit app enables interactive exploration by allowing users to select a month via a dropdown menu. The accident map updates dynamically to display a heatmap overlaid with individual accident points for the selected month. This setup allows users to visually assess whether accident hotspots shift or intensify in different parts of the city from winter to summer months, though no major seasonal migrations of hotspots are apparent in the available data.
+
+## Future work
+Besides the effect Thursday / Friday, would the meteorologic conditions affect accidents? Would the hourly distribution be different in December / January (winter) and July / August (Summer)? 
+
+
+Maia, September 2025
+Pedro Pimenta
---
+++ app.py
@@ -0,0 +1,135 @@
+import streamlit as st
+import pandas as pd
+import geopandas as gpd
+import folium
+from shapely.geometry import Point
+from streamlit_folium import st_folium
+from folium.plugins import HeatMap
+
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+
+
+# Set page config
+st.set_page_config(page_title="Lisbon Road Accidents (Pedro Pimenta)", layout="wide")
+
+st.title("Lisbon Road Accidents")
+st.html("<h2>Capstone Project</h2>")
+st.html("Developed in the context of \"Tools and Techniques for Geospatial Machine Learning\" (Factual <a href='https://factual-academy.com/bundle/aim4mobility' target='*'>AIM4Mobility</a>)")
+
+st.html("<hr color=gray>" )
+
+col1, col2 = st.columns(2)
+# Load data
+df = pd.read_csv("data/Road_Accidents_Lisbon.csv")
+
+with col1:
+    st.caption("An example of the vailable data:")
+    st.title("dataset sample")
+    st.dataframe(df.head()) 
+with col2:
+    st.caption("Just checking monthly dependency:")
+
+    st.title('Number of Accidents by month')
+
+    import calendar
+    # list of month names using calendar module
+    month_order = list(calendar.month_name)[1:]
+    month_name = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    month_order = [m[:3] for m in month_name]
+    month_counts = df['month'].value_counts().reindex(month_order).fillna(0)
+    month_counts.index = pd.CategoricalIndex(month_counts.index, categories=month_order, ordered=True)
+    month_counts = month_counts.sort_index()
+    st.bar_chart(month_counts)
+    st.caption("No meaningful pattern was identified.")
+
+st.html("<hr color=gray>")
+
+heatmap_data = df.pivot_table(index='weekday', columns='hour', aggfunc='size', fill_value=0)
+
+# Sort weekdays in natural order if needed
+order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+heatmap_data = heatmap_data.reindex(order)
+
+# Plot the heatmap
+fig, ax = plt.subplots(figsize=(8,5))
+sns.heatmap(
+    heatmap_data, cmap='YlGnBu', annot=True, fmt='d', ax=ax,
+    annot_kws={"size": 8},  # smaller annotation font size
+    cbar_kws={'label': 'Count'}
+    )
+
+# Add titles and labels
+ax.set_title('Occurrences by Weekday and Hour')
+ax.set_xlabel('Hour of Day')
+ax.set_ylabel('Weekday')
+
+# Display plot in Streamlit
+st.caption("Heatmap for hourly vs daily patterns")
+st.pyplot(fig)
+st.caption("This heatmap shows the biggest concentration of accidents happens from monday to friday - mainly Thursday and Friday - by the end of day.")
+
+
+st.html("<hr color=orange>")
+
+st.html("Explore how accident locations change throughout the year.<br>"
+        " The visualization combines density heatmaps (showing concentration patterns) with individual points (exact locations) for complete spatial insight."
+    )
+
+# Create dropdown
+selected_value = st.selectbox(
+    "Select one month:",
+    options=df['month'].unique(),  # Replace with your column name
+    index=0
+)
+
+filtered_df = df[df['month'] == selected_value]
+
+
+# Convert to GeoDataFrame
+gdf = gpd.GeoDataFrame(
+    filtered_df,
+    geometry=[Point(xy) for xy in zip(filtered_df.longitude, filtered_df.latitude)],
+    crs="EPSG:4326"
+)
+
+# Create base map centered on Lisbon
+center = [gdf["latitude"].mean(), gdf["longitude"].mean()]
+m = folium.Map(location=center, zoom_start=12, tiles="CartoDB Positron")
+
+
+# Prepare heatmap points
+heat_data = [[row['latitude'], row['longitude']] for _, row in filtered_df.iterrows()]
+
+# Create and center map
+center = [filtered_df['latitude'].mean(), filtered_df['longitude'].mean()]
+m = folium.Map(location=center, zoom_start=12, tiles='CartoDB Positron')
+HeatMap(heat_data, radius=12).add_to(m)
+
+
+# Add accident points
+for _, row in gdf.iterrows():
+    folium.CircleMarker(
+        location=[row["latitude"], row["longitude"]],
+        radius=4,
+        color="red",
+        fill=True,
+        fill_opacity=0.6,
+        popup=f"ID: {row['id']}<br>Hour: {row['hour']}<br>Fatalities: {row['fatalities_30d']}"
+    ).add_to(m)
+
+
+#m
+
+
+# Render map in Streamlit
+st_data = st_folium(m, width=800, height=600)
+
+# Display notice
+st.markdown( "**Important Notice:** Use of this data restricted for educational purposes within this course only." )
+
+st.html( 
+"<hr color=lime><i>Maia, Setembro 2025<br>Pedro Pimenta (<a href='https://www.linkedin.com/in/pedropimenta/' target='_'>https://www.linkedin.com/in/pedropimenta/</a>)"
+"<hr color=lime>"
+)
